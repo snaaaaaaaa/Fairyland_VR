b7abf77603570d386d57d6bbf351b55506d95d7e
Alyssa Wright <awri9576@uni.sydney.edu.au>
2026-04-29 16:49:36 +1000

    Added enemy sound (untested) and wand effects

--- Assets/Alyssa-Assets/audio/PlaySoundOnCollision.cs
+++ Assets/Alyssa-Assets/audio/PlaySoundOnCollision.cs
@@ -6,11 +6,13 @@ public class PlaySoundOnCollision : MonoBehaviour
 {
     public AudioSource soundEffect; // Assign your audio source in the Inspector
 
+    public string collisionTag = "";
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("collision npc");
         // Only trigger if the Main Camera enters this object's trigger collider
-        if (!other.CompareTag("MainCamera")) return;
+        if (!other.CompareTag(tag)) return;
 
         // Play the sound effect
         Debug.Log("playbonk");
