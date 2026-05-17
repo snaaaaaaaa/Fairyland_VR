36792aab6ca31baa8a9f2a0c17ffc383ba363294
Alyssa Wright <awri9576@uni.sydney.edu.au>
2026-05-17 18:39:01 +1000

    continuing creating tutorial

--- Assets/Landing-Assets/Scripts/LandingSceneObjects.cs
+++ Assets/Landing-Assets/Scripts/LandingSceneObjects.cs
@@ -6,10 +6,13 @@ public class LandingSceneObjects : MonoBehaviour
 {
     public GameObject wandObject;
     [SerializeField] private GameObject wand;
-    public GameObject rayInteractorLeft;
-    public GameObject rayInteractorRight;
+    public GameObject[] rayInteractors;
+    // public GameObject rayInteractorLeft;
+    // public GameObject rayInteractorRight;
 
-    public GameObject abc;
+    public GameObject clover;
+    public GameObject key;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +23,7 @@ public class LandingSceneObjects : MonoBehaviour
         // }
 
         // enableWand(false);
+        // defineRayInteractors();
     }
 
     // Update is called once per frame
@@ -30,6 +34,8 @@ public class LandingSceneObjects : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Collided with: " + other.gameObject.name);
+        defineRayInteractors(rayInteractors);
+
         // Check if the colliding object is tagged as gamecontroller (left or right hand controller)
         if (other.CompareTag("GameController"))
         {
@@ -47,11 +53,19 @@ public class LandingSceneObjects : MonoBehaviour
 
         // Debug.Log(rayInteractors.Length);
 
-
+        defineRayInteractors(rayInteractors);
 
         wand.SetActive(enable);
-        rayInteractorLeft.SetActive(enable);
-        rayInteractorRight.SetActive(enable);
+
+        foreach (GameObject ray in rayInteractors)
+        {
+            ray.SetActive(enable);
+        }
+
+        // rayInteractorLeft.SetActive(enable);
+        // rayInteractorRight.SetActive(enable);
+
+
 
         if (enable)
         {
@@ -59,5 +73,15 @@ public class LandingSceneObjects : MonoBehaviour
         }
 
 
+    }
+
+    public void defineRayInteractors(GameObject[] rayInteractors)
+    {
+        rayInteractors = GameObject.FindGameObjectsWithTag("RayInteractor");
+        Debug.Log("defineRayInteractors: rayInteractors Length = " + rayInteractors.Length);
+        for (int i = 0; i < rayInteractors.Length; i++)
+        {
+            Debug.Log("rayInteractors[" + i + "] = " + rayInteractors[i].name);
+        }
     }
 }
